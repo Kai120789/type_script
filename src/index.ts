1af105e585b07d378eb8bb3d3ff93a9c311ba984
Kai120789 <zoomer120789@gmail.com>
2025-02-23 16:45:21 +0300

    feat: add test route with types

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import express from "express"
+import express, { NextFunction, Request, Response } from "express"
 
 const app = express()
 
 const PORT = 3000
+
+app.get('/api/users', (request: Request, response: Response, next: NextFunction) => {
+    response.send([])
+})
 
 app.listen(PORT, () => {
     console.log(`Running on Port ${PORT}`)
