28332bf46ff9f385e39688df70bd28a7c75fa5c8
Kai120789 <zoomer120789@gmail.com>
2025-02-24 17:39:03 +0300

    feat: response and query-params

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { CreateUserDto } from "../dto/CreateUser.dto";
+import { CreateUserQueryParams } from "../types/query-params";
+import { User } from "../types/response";
 
 export function getUsers(request: Request, response: Response) {
     response.send([])
@@ -9,6 +11,10 @@ export function getUserById(request: Request, response: Response) {
     response.send({})
 }
 
-export function createUser(request: Request<{}, {}, CreateUserDto>, response: Response) {
-    request.body
+export function createUser(request: Request<{}, {}, CreateUserDto, CreateUserQueryParams>, response: Response<User>) {
+    response.status(201).send({
+        id: 1,
+        username: "d",
+        email: "hj"
+    })
 }
